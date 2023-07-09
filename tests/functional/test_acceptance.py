d03462e069a36d1efa26610978b138db3c0bdcaa
dev-com2020 <kwp@tlen.pl>
2023-07-09 10:13:48 +0200

    09.07 10:13

--- tests/functional/test_acceptance.py
+++ tests/functional/test_acceptance.py
@@ -9,3 +9,14 @@ class TestAddingEntries:
         assert app._contacts == [
             ("Jan Kowalski", "123456789")
         ]
+
+    def test_surnames_with_spaces(self):
+        app = contacts.Application()
+        app.run("contacts add Ludwik Mariusz Kowalski 123456789")
+        app.run("contacts add Jan Kowalski-Kowalski 123456789")
+
+
+        assert app._contacts == [
+            ("Ludwik Mariusz Kowalski", "123456789"),
+            ("Jan Kowalski-Kowalski", "123456789")
+        ]
