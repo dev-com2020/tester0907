9c9189ade8b6a00a1d052b3845c38a9c92ede000
dev-com2020 <kwp@tlen.pl>
2023-07-09 15:13:17 +0200

    09.07 15:13

---
+++ tests/unit/test_sel1.py
@@ -0,0 +1,42 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+def test_basic_options_chrome():
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+    time.sleep(2)
+    driver.quit()
+
+# def test_basic_options_firefox():
+#     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+#     driver.get("https://www.google.com")
+#     time.sleep(2)
+#     driver.quit()
+
+# def test_basic_options_ie():
+#     driver = webdriver.Ie()
+#     driver.get("https://www.google.com")
+#     time.sleep(2)
+#     driver.quit()
+
+def test_basic_options_edge():
+    driver = webdriver.Edge()
+    driver.get("https://www.google.com")
+    time.sleep(2)
+    driver.quit()
+
+# def test_basic_options_opera():
+#     driver = webdriver.Opera()
+#     driver.get("https://www.google.com")
+#     time.sleep(2)
+#     driver.quit()
+
+# def test_basic_options_safari():
+#     driver = webdriver.Safari()
+#     driver.get("https://www.google.com")
+#     time.sleep(2)
+#     driver.quit()
+#
